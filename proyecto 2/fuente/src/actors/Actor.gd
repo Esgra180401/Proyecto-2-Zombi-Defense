035d6edcd2d67c3d0d8fd9d783d959b022131a40
Esteban Granda <180401@estudiantec.cr>
2020-10-24 21:27:42 -0600

    interaccion de colicion

--- proyecto 2/fuente/src/actors/Actor.gd
+++ proyecto 2/fuente/src/actors/Actor.gd
@@ -2,5 +2,5 @@ extends KinematicBody2D
 class_name Actor
 
 func _physics_process(delta: float) -> void:
-	var velocity: = Vector2(0,-100)
+	var velocity: = Vector2(0,-50)
 	move_and_slide(velocity)
