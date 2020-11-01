7f9536674fe0d1ca8d37e1e9f9d93fcf487eba6e
Erick <ag.erick@hotmail.com>
2020-10-31 20:21:55 -0600

    Rotacion arreglada

--- proyecto 2/fuente/src/actors/player_movement.gd
+++ proyecto 2/fuente/src/actors/player_movement.gd
@@ -17,19 +17,19 @@ func _physics_process(delta):
 		
 		if Input.is_action_pressed("ui_up"):
 			motion_vector = Vector2( 0, -1)
-			#rotation_degrees = 0
+			rotation_degrees = 0
 			tile_size = 37
 		if Input.is_action_pressed("ui_down"):
 			motion_vector = Vector2( 0, 1)
-			#rotation_degrees = 180
+			rotation_degrees = 180
 			tile_size = 37
 		if Input.is_action_pressed("ui_left"):
 			motion_vector = Vector2( -1, 0)
-			#rotation_degrees = 260
+			rotation_degrees = 260
 			tile_size = 39
 		if Input.is_action_pressed("ui_right"):
 			motion_vector = Vector2( 1, 0)
-			#rotation_degrees = 90
+			rotation_degrees = 90
 			tile_size = 39
 
 		if motion_vector != Vector2():
